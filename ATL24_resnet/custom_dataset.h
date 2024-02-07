f34f9db396ac0d3fc17365b3fcd8e4546bdf627c
Jeff Perry <jsp@utexas.edu>
2024-02-07 13:31:05 -0600

    Add second custom dataset

--- ATL24_resnet/custom_dataset.h
+++ ATL24_resnet/custom_dataset.h
@@ -3,6 +3,7 @@
 #include "ATL24_resnet/pgm.h"
 #include "ATL24_resnet/utils.h"
 #include "ATL24_utils/dataframe.h"
+#include <map>
 
 namespace ATL24_resnet
 {
@@ -59,10 +60,7 @@ class classified_point_dataset : public torch::data::datasets::Dataset<classifie
             datasets[i] = convert_dataframe (df);
 
             if (verbose)
-            {
                 clog << datasets[i].size () << " points read" << endl;
-                clog << "Sorting points" << endl;
-            }
 
             // Sort them by X
             sort (datasets[i].begin (), datasets[i].end (),
@@ -97,6 +95,167 @@ class classified_point_dataset : public torch::data::datasets::Dataset<classifie
 
             // Save them
             sample_indexes.insert (sample_indexes.end (), i.second.begin (), i.second.end ());
+        }
+
+        // Randomize the order
+        shuffle (sample_indexes.begin (), sample_indexes.end (), rng);
+
+        // Show results
+        if (verbose)
+            clog << "Total samples: " << sample_indexes.size () << endl;
+    }
+
+    Example get (size_t index)
+    {
+        using namespace std;
+        using namespace viper::raster;
+
+        assert (index < sample_indexes.size ());
+
+        // Create image raster
+        const auto dataset_index = sample_indexes[index].dataset_index;
+        const auto point_index = sample_indexes[index].point_index;
+        assert (dataset_index < datasets.size ());
+        assert (point_index < datasets[dataset_index].size ());
+        auto p = create_raster (
+            datasets[dataset_index],
+            point_index,
+            patch_rows,
+            patch_cols,
+            aspect_ratio,
+            rp,
+            rng);
+
+        // Create image Tensor from raster
+        auto grayscale = torch::from_blob (&p[0],
+                { static_cast<int> (patch_rows), static_cast<int> (patch_cols) },
+                torch::kUInt8).to(torch::kFloat);
+
+        // Create label Tensor
+        const long cls = label_map.at (datasets[dataset_index][point_index].cls);
+        auto label = torch::full ({1}, cls);
+
+        return {grayscale.clone(), label.clone()};
+    }
+
+    torch::optional<size_t> size() const
+    {
+        return sample_indexes.size ();
+    }
+};
+
+class classified_point_dataset2 : public torch::data::datasets::Dataset<classified_point_dataset2>
+{
+    using Example = torch::data::Example<>;
+    std::vector<std::vector<ATL24_resnet::classified_point2d>> datasets;
+    std::vector<sample_index> sample_indexes;
+    size_t patch_rows;
+    size_t patch_cols;
+    double aspect_ratio;
+    regularization_params rp;
+    std::default_random_engine &rng;
+
+    public:
+    classified_point_dataset2 (const std::vector<std::string> &fns,
+        const size_t patch_rows,
+        const size_t patch_cols,
+        const double aspect_ratio,
+        const regularization_params &rp,
+        const size_t min_samples_per_class,
+        const size_t max_samples_per_class,
+        const bool verbose,
+        std::default_random_engine &rng)
+        : patch_rows (patch_rows)
+        , patch_cols (patch_cols)
+        , aspect_ratio (aspect_ratio)
+        , rp (rp)
+        , rng (rng)
+    {
+        using namespace std;
+
+        // One dataset per input file
+        datasets.resize (fns.size ());
+
+        // Open each file
+        for (size_t i = 0; i < fns.size (); ++i)
+        {
+            const auto fn = fns[i];
+
+            if (verbose)
+                clog << "Reading " << fn << endl;
+
+            // Read the points
+            const auto df = ATL24_utils::dataframe::read (fn);
+
+            // Convert it to the correct format
+            datasets[i] = convert_dataframe (df);
+
+            if (verbose)
+                clog << datasets[i].size () << " points read" << endl;
+
+            // Sort them by X
+            sort (datasets[i].begin (), datasets[i].end (),
+                [](const auto &a, const auto &b)
+                { return a.x < b.x; });
+
+            // Count each class
+            unordered_map<size_t,size_t> cls_sizes;
+
+            for (size_t j = 0; j < datasets[i].size (); ++j)
+                ++cls_sizes[datasets[i][j].cls];
+
+            for (auto j : cls_sizes)
+            {
+                if (verbose)
+                    clog << "Class\t" << j.first << "\t"
+                        << j.second << " photons" << endl;
+
+                // If there aren't enough, skip it
+                if (j.second < min_samples_per_class)
+                    continue;
+
+                // Determine how many to add
+                const size_t total_photons = j.second;
+                const size_t total_to_add = std::min (total_photons, max_samples_per_class);
+
+                if (verbose)
+                    clog << "Adding\t" << total_to_add << " photons" << endl;
+
+                // 0, 1, 2, ..., total_photons-1
+                vector<size_t> indexes (total_photons);
+                iota (indexes.begin (), indexes.end (), 0);
+
+                // Randomize the order
+                shuffle (indexes.begin (), indexes.end (), rng);
+
+                // Only use as many as we need
+                indexes.resize (total_to_add);
+
+                // Append them to the end
+                for (auto k : indexes)
+                    sample_indexes.push_back (sample_index {i, k});
+            }
+        }
+
+        // Get sample indexes of points in each class
+        std::map<size_t,size_t> cls_counts;
+
+        for (size_t i = 0; i < sample_indexes.size (); ++i)
+        {
+            const size_t dindex = sample_indexes[i].dataset_index;
+            assert (dindex < datasets.size ());
+            const size_t pindex = sample_indexes[i].point_index;
+            assert (pindex < datasets[dindex].size ());
+            const auto cls = datasets[dindex][pindex].cls;
+            ++cls_counts[cls];
+        }
+
+        // Show results
+        if (verbose)
+        {
+            clog << "Class counts:" << endl;
+            for (auto i : cls_counts)
+                clog << "\t" << i.first << "\t" << i.second << endl;
         }
 
         // Randomize the order
