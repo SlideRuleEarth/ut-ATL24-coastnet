978b62ebb898aff37ad0314b1b5051ddb5d55a4a
Jeff Perry <jsp@utexas.edu>
2024-03-13 16:10:05 -0500

    Add verbose output

--- ATL24_coastnet/blunder_detection.h
+++ ATL24_coastnet/blunder_detection.h
@@ -9,6 +9,8 @@ namespace ATL24_coastnet
 template<typename T,typename U>
 T blunder_detection (T p, const U &args)
 {
+    using namespace std;
+
     // Reclassify photons using heuristics
     if (p.empty ())
         return p;
@@ -16,6 +18,12 @@ T blunder_detection (T p, const U &args)
     // Count classes
     const size_t total_surface = count_predictions (p, sea_surface_class);
     const size_t total_bathy = count_predictions (p, bathy_class);
+
+    if (args.verbose)
+    {
+        clog << "Total surface photons: " << total_surface << endl;
+        clog << "Total bathy photons: " << total_bathy << endl;
+    }
 
     // If there's no bathy or surface, there's nothing to do
     if (total_surface == 0 && total_bathy == 0)
@@ -51,6 +59,9 @@ T blunder_detection (T p, const U &args)
         if (p[i].z < args.bathy_min_elevation)
             p[i].cls = 0;
     }
+
+    if (total_surface == 0)
+        return p;
 
     // Bathy can only be 'under' the surface if there is a surface photon nearby
     const auto nearby_surface_indexes = get_nearest_along_track_prediction (p, 41);
