247aaacdc959f5116d79ec9454fe12298c395c85
Jeff Perry <jsp@utexas.edu>
2024-03-21 10:43:46 -0500

    Add weighted scoring values

--- get_coastnet_surface_scores.sh
+++ get_coastnet_surface_scores.sh
@@ -4,9 +4,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-printf "Surface Acc = "
+printf "Average Surface Acc = "
 cat $1 | grep "^41" | cut -f 2 | grep -v nan | datamash --format="%0.3f" mean 1
-printf "Surface F1 = "
+printf "Average Surface F1 = "
 cat $1 | grep "^41" | cut -f 3 | grep -v nan | datamash --format="%0.3f" mean 1
-printf "Surface BA = "
+printf "Average Surface BA = "
 cat $1 | grep "^41" | cut -f 4 | grep -v nan | datamash --format="%0.3f" mean 1
+printf "Weighted Surface Acc = "
+cat $1 | grep "weighted_acc" | cut -f 3 -d ' ' | grep -v nan | datamash --format="%0.3f" mean 1
+printf "Weighted Surface F1 = "
+cat $1 | grep "weighted_F1" | cut -f 3 -d ' ' | grep -v nan | datamash --format="%0.3f" mean 1
+printf "Weighted Surface BA = "
+cat $1 | grep "weighted_bal_acc" | cut -f 3 -d ' ' | grep -v nan | datamash --format="%0.3f" mean 1
