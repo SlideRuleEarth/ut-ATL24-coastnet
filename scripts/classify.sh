947d618a47de8725d6ad529c832bd322aa543cb8
Jeff Perry <jsp@utexas.edu>
2024-07-17 10:32:15 -0500

    Rename model parameters

--- scripts/classify.sh
+++ scripts/classify.sh
@@ -8,6 +8,6 @@ mkdir -p predictions
 for n in $(seq 0 4)
 do
     parallel --lb --dry-run \
-        "build/release/classify --verbose --num-classes=7 --network-filename=coastnet_network-${n}.pt --results-filename=predictions/{/.}_results-${n}.txt < {} > predictions/{/.}_classified_${n}.csv" \
+        "build/release/classify --verbose --num-classes=7 --model-filename=coastnet_model-${n}.json --results-filename=predictions/{/.}_results-${n}.txt < {} > predictions/{/.}_classified_${n}.csv" \
         :::: coastnet_test_files-${n}.txt
 done
